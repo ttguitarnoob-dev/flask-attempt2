dfb20de041942b13fb8218be5948bcbcecc2ca6e
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-10-31 10:33:50 -0500

    able to create a route that querys the postgres db

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+import psycopg2
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="10.24.24.218",
+        database = "testdb",
+        user= "postgres",
+        password= "Bungfodder123")
+
+    return conn
+
+
+
 @app.route('/')
 def index():
-    return "OMG INDEX"
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM smellass;')
+    bookass = cur.fetchall()
+    cur.close()
+    conn.close()
+    return bookass
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host = '0.0.0.0', port = 5000, debug=True)
